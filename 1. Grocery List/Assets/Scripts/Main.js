1e97c98cee658ec04edddf7476bc71f56c70a690
Alexander Rhett Crammer <alexander2475914@gmail.com>
2015-05-04 23:24:01 -0400

    Add Functionality
    Angular.min.js »
        Move to `Assets/Scripts`
    
    Main.js »
        Move script from `index.html`
    
    - Add a list of a few pre-defined groceries
    - Allow the user to provide new grocery ideas to the list
    - Allow the user to remove groceries from the list
    
    Now I’m going to work on the styles. “(‘^-^)”

---
+++ 1. Grocery List/Assets/Scripts/Main.js
@@ -0,0 +1,16 @@
+/*  Main.js
+    Grocery List
+    Monday, 4 May, 2015
+    Alexander Rhett Crammer  */
+
+var GroceryList = angular.module('GroceryList', []);
+GroceryList.controller('ListController', ['$scope', function ($scope) {
+  /* Properties */
+  $scope.groceries = ["Soymilk","Eggs","Pasta","Tomatoes","Basil"];
+  
+  /* Methods */
+  $scope.subtractGrocery = function (named) {
+    var namedGroceryIndex = $scope.groceries.indexOf(named);
+    $scope.groceries.splice(namedGroceryIndex, 1);
+  }
+}]);
